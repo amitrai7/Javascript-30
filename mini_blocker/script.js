f0e98566cfa0052fb132334518b5ce46ac7650d2
amitrai7 <raiamit743@gmail.com>
2020-10-02 14:57:57 +0530

    Initial commit

---
+++ mini_blocker/script.js
@@ -0,0 +1,19 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+function jump(){
+    if (character.classList.contains("animate")){return}
+    character.classList.add("animate");
+    setTimeout(() => {
+        character.classList.remove("animate")
+    }, 700);
+}
+
+var checkDead = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+    if(blockLeft<20 && blockLeft>0 && characterTop>=185){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("Better luck next time!");
+    }
+},5);
